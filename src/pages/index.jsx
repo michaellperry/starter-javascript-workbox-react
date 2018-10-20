e6deb9257cc02d7d7f78c51cdff0035b13de76cc
Michael L Perry <michael@qedcode.com>
2018-10-19 22:53:37 -0500

    Gatsby documentation site.

---
+++ src/pages/index.jsx
@@ -0,0 +1,123 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import styled from 'styled-components'
+
+import SEO from '../components/SEO'
+import config from '../../data/SiteConfig'
+import CtaButton from '../components/CtaButton'
+import Navigation from '../components/Layout/Navigation'
+
+class Index extends React.Component {
+  render() {
+    const allSEOMarkdown = this.props.data.allMarkdown.edges
+
+    return (
+      <div className="index-container">
+        <Helmet title={config.siteTitle} />
+        <SEO postEdges={allSEOMarkdown} />
+        <main>
+          <IndexHeadContainer>
+            <Navigation />
+            <Hero>
+              <img src={config.siteLogo} width="150px" alt="" />
+              <h1>{config.siteTitle}</h1>
+              <h4>{config.siteDescription}</h4>
+            </Hero>
+          </IndexHeadContainer>
+          <BodyContainer>
+            <h2>A Gatsby Template for Content</h2>
+            <p>
+              Made for modern documentation sites. Table of Contents
+              automatically generated from markdown files.{' '}
+            </p>
+            <CtaButton to={'/lesson-one'}>See Your First Post</CtaButton>
+
+            <div className="contributors">
+              <p>
+                Created by Eric Windmill.{' '}
+                <a href="https:twitter.com/ericwindmill">
+                  You should follow him on Twitter.
+                </a>{' '}
+                Also, <a href="https://github.com/Levino">Levin Keller</a> for
+                making it better than I could{"'"}ve alone.
+              </p>
+            </div>
+          </BodyContainer>
+        </main>
+      </div>
+    )
+  }
+}
+
+export default Index
+
+const IndexHeadContainer = styled.div`
+  background: ${props => props.theme.brand};
+  padding: ${props => props.theme.sitePadding};
+  text-align: center;
+`
+
+const Hero = styled.div`
+  padding: 50px 0;
+  & > h1 {
+    font-weight: 600;
+  }
+`
+
+const BodyContainer = styled.div`
+  padding: ${props => props.theme.sitePadding};
+  max-width: ${props => props.theme.contentWidthLaptop};
+  margin: 0 auto;
+
+  .contributors {
+    max-width: 400px;
+    margin: 100px auto 0;
+  }
+`
+
+/* eslint no-undef: "off" */
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdown: allMarkdownRemark(
+      limit: 2000
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          excerpt
+          timeToRead
+          frontmatter {
+            title
+            tags
+            cover
+            date
+          }
+        }
+      }
+    }
+    posts: allMarkdownRemark(
+      limit: 2000
+      filter: { frontmatter: { type: { eq: "post" } } }
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          excerpt
+          timeToRead
+          frontmatter {
+            title
+            tags
+            cover
+            date
+          }
+        }
+      }
+    }
+  }
+`
