1589ffabf7738e772de1879789244b86e36c937c
Michael L Perry <michael@qedcode.com>
2018-10-21 17:56:14 -0500

    Center the call to action button.

--- src/components/CtaButton.jsx
+++ src/components/CtaButton.jsx
@@ -6,7 +6,7 @@ class ctaButton extends Component {
   render() {
     const { children } = this.props
     return (
-      <Link style={{ border: 'none' }} to={this.props.to}>
+      <Link className="cta" style={{ border: 'none' }} to={this.props.to}>
         <ButtonContainer>{children}</ButtonContainer>
       </Link>
     )
