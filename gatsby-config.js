ff62af3f613eddba786fe919b0183b1e5ec08db8
Michael L Perry <michael@qedcode.com>
2019-01-14 13:01:28 -0600

    Enabled Google Analytics.

--- gatsby-config.js
+++ gatsby-config.js
@@ -84,5 +84,11 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-389401-11",
+      },
+    },
   ],
 }
