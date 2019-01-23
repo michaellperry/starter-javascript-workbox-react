95593cbacbce65073256ffefbf56f22fbffb13ae
Michael L Perry <michael@qedcode.com>
2019-01-23 13:56:05 -0600

    Describe mutable properties in the overview.

--- src/examples/intro/overview.ts
+++ src/examples/intro/overview.ts
@@ -25,6 +25,8 @@ async function factsHavePredecessors(qedcode) {
 
     console.log(JSON.stringify(post, null, 2));
     console.log('');
+
+    return post;
 }
 
 async function queryForSuccessors(qedcode) {
@@ -46,8 +48,72 @@ function postInSite(s) {
     });
 }
 
+async function mutablePropertiesAreSuccessors(post) {
+    console.log('Since facts are immutable, you can\'t store mutable properties in them.');
+    console.log('The blog post as defined above has no title.');
+    console.log('That\'s because the author should be able to change the title.');
+    console.log('In Jianga, we represent mutable properties as successors');
+
+    const title = await j.fact({
+        type: 'Blog.Post.Title',
+        value: 'Required Reading: Pat Helland, Immutability Changes Everything',
+        post: post
+    });
+
+    console.log(JSON.stringify(title, null, 2));
+    console.log('');
+
+    return title;
+}
+
+async function changeThePropertyByCreatingANewFact(post, title) {
+    console.log('To modify the value of a mutable property, you have to create a new fact.');
+    console.log('This fact should refer back to the previous version.');
+    console.log('We typically do this in an array:');
+
+    const newTitle = await j.fact({
+        type: 'Blog.Post.Title',
+        value: 'I Really Think You Should Read Pat\'s Paper',
+        post: post,
+        prior: [title]
+    });
+
+    console.log(JSON.stringify(newTitle, null, 2));
+    console.log('');
+}
+
+async function queryForTheCurrentValue(post) {
+    console.log('To get the current value of a mutable property, you run a query.');
+    console.log('That query searches for facts that do *not* appear as predecessors:');
+    
+    const title: any = await j.query(post, j.for(titleOfPost));
+
+    console.log(JSON.stringify(title.map(t => t.value), null, 2));
+    console.log('Notice that the result is returned as an array, but it only has the');
+    console.log('recent version.');
+    console.log('Can you think of a scenario in which you could get more than one value?');
+    console.log('');
+}
+
+function titleOfPost(p) {
+    return j.match({
+        type: 'Blog.Post.Title',
+        post: p
+    }).suchThat(titleIsCurrent);
+}
+
+function titleIsCurrent(t) {
+    return j.notExists({
+        type: 'Blog.Post.Title',
+        prior: [t]
+    });
+}
+
 (async () => {
     const qedcode = await dataIsStoredAsFacts();
-    await factsHavePredecessors(qedcode);
+    const post = await factsHavePredecessors(qedcode);
     await queryForSuccessors(qedcode);
+    const title = await mutablePropertiesAreSuccessors(post);
+    await changeThePropertyByCreatingANewFact(post, title);
+    await queryForTheCurrentValue(post);
 })();
